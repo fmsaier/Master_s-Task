26124a9ba88fc3eef7294481dd3104a0c703bb56
LiobraveryPlum <145110043+LiobraveryPlum@users.noreply.github.com>
2024-03-25 16:00:16 +0800

    更新关卡

--- Assets/Lio's Assets/Scripts/Duct.cs
+++ Assets/Lio's Assets/Scripts/Duct.cs
@@ -22,31 +22,32 @@ namespace Lio
         {
             export = transform.GetChild(0);
             number = 0;
-            runing = begin;
+            //runing = begin;
+            runing = true;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            //if (Input.GetMouseButtonDown(0))
+            //{
+            //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //    RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 
-                if (hit.collider != null)
-                {
-                    if (hit.collider.gameObject == gameObject)
-                    {
-                        runing = !runing;
-                    }
-                }
-            }
+            //    if (hit.collider != null)
+            //    {
+            //        if (hit.collider.gameObject == gameObject)
+            //        {
+            //            runing = !runing;
+            //        }
+            //    }
+            //}
 
             if(number>0)
             {
                 GameObject ob = Instantiate(smog, export.transform.position, Quaternion.identity);
                 Rigidbody2D rd = ob.GetComponent<Rigidbody2D>();
-                float dir = Random.Range(0f, 180f);
+                float dir = Random.Range(45f, 135f);
                 Vector3 direction = Quaternion.Euler(new Vector3(0, 0, dir)) * Vector3.right;
                 Vector3 vel = direction * Random.Range(velRange.x, velRange.y);
                 rd.velocity = vel;
